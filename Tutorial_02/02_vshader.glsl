537b7d9db1e9006813bea72a56e3ee310b8f3a27
Parag Chaudhuri <1897526+paragchaudhuri@users.noreply.github.com>
2020-10-27 11:22:30 +0530

    Added a camera

--- Tutorial_02/02_vshader.glsl
+++ Tutorial_02/02_vshader.glsl
@@ -3,9 +3,10 @@
 in vec4 vPosition;
 in vec4 vColor;
 out vec4 color;
+uniform mat4 ModelViewProjectMatrix;
 
 void main () 
 {
-  gl_Position = vPosition;
+  gl_Position = ModelViewProjectMatrix * vPosition;
   color = vColor;
 }
