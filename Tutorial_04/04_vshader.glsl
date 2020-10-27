7e83aa25b7d5dbeabb62bd66ba08c53406268127
Parag Chaudhuri <1897526+paragchaudhuri@users.noreply.github.com>
2020-10-27 12:01:51 +0530

    Update 04_vshader.glsl

--- Tutorial_04/04_vshader.glsl
+++ Tutorial_04/04_vshader.glsl
@@ -1,12 +1,12 @@
-#version 440
+#version 330
 
 in vec4 vPosition;
 in vec4 vColor;
 out vec4 color;
-uniform mat4 uModelViewMatrix;
+uniform mat4 uModelViewProjectMatrix;
 
 void main (void) 
 {
-  gl_Position = uModelViewMatrix * vPosition;
+  gl_Position = uModelViewProjectMatrix * vPosition;
   color = vColor;
 }
