f5c487868ebeaac995a9c396f5753c0ae3927790
Parag Chaudhuri <1897526+paragchaudhuri@users.noreply.github.com>
2020-10-27 11:21:39 +0530

    Update 02_colorcube.hpp

--- Tutorial_02/02_colorcube.hpp
+++ Tutorial_02/02_colorcube.hpp
@@ -15,8 +15,12 @@
 #define DELETE 127
 
 //Include Files
+#include "glm/vec3.hpp"
 #include "glm/vec4.hpp"
 #include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
 #include "gl_framework.hpp"
 #include "shader_util.hpp"
 
